0402e22c3b66012cbe5e6152d59c01a997ee230c
dizzy-cool <965136370@qq.com>
2019-11-19 15:29:08 +0800

    修改了v0.2版本的交换关系为相邻元素比较 完成注释的补充

--- bubbleSort/Project1/Project1/BubbleSort.c
+++ bubbleSort/Project1/Project1/BubbleSort.c
@@ -4,16 +4,20 @@
 int BubbleSort(int arr[], int size) {
 	//bound表示的是边界坐标
 	//[ 0 ,bound )表示已经排序的区间
-	//( bound , size-1]表示未排序区间
+	//[bound , size-1)表示未排序区间
+	//每次找一个最小值放到前面去,也就一位置已排序区间多一个元素,
+	//待排序区间少一个元素
 	for (int bound = 0; bound < size; bound++) {
 		//再来一重循环用来遍历数组
 		//按照升序来排列,最大元素在最后面
+		//这重循环用来完成:找到当前待排序区间中的最小值,并放在bound位置上
+
 		for (int cur = size - 1; cur > bound; cur--) {
-			//判断调换,将不符合升序规则的元素调换位置
-			if (arr[bound] > arr[cur]) {
+			//判断相邻元素,如果不符合升序规则,就交换两个元素
+			if (arr[cur - 1] > arr[cur]) {
 				int temp = 0;
-				temp = arr[bound];
-				arr[bound] = arr[cur];
+				temp = arr[cur - 1];
+				arr[cur - 1] = arr[cur];
 				arr[cur] = temp;
 			}
 		}
