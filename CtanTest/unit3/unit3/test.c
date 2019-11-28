3180690107c78c9923bf7c61a08d791fe617db20
dizzy-cool <965136370@qq.com>
2019-11-28 17:55:14 +0800

    unit3完成银行年利

--- CtanTest/unit3/unit3/test.c
+++ CtanTest/unit3/unit3/test.c
@@ -7,10 +7,24 @@ double increase(double r, int year) {
 	return p;
 }
 
+void dispostion(double money, int year) {
+	switch (year) {
+	case 5:
+		money = money * pow((1 + 0.0585),5);
+		printf("money = %lf\n", money);
+		break;
+	case 2:
+		money = money * pow((1 + 0.0468), 2);
+		money = money * pow((1 + 0.0540), 3);
+		printf("money = %lf\n", money);
+		break;
+	}
+	return ;
+}
 int main(void){
-	
+	dispostion(1000.0, 5);
 	 
-	printf("%lf ", increase(0.10, 10));
+	//printf("%lf ", increase(0.10, 10));
 	system("pause");
 	return 0;
 }
